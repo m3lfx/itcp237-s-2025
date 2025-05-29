e498cc1124e628e1d5dd3bf7a53e985143276220
rommel dalisay <rommelmdalisay@gmail.com>
2025-05-29 14:16:32 +0800

    charts

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -7,7 +7,7 @@ use DB;
 
 class DashboardController extends Controller
 {
-   public function addressChart()
+    public function addressChart()
     {
         $customer = DB::table('customer')
             ->groupBy('addressline')
@@ -18,6 +18,42 @@ class DashboardController extends Controller
         $labels = (array_keys($customer));
         $data = array_values($customer);
         // dd($customer, $data, $labels);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
+
+    public function salesChart()
+    {
+        $sales = DB::table('item as i')
+            ->join('orderline as ol', 'i.item_id', '=', 'ol.item_id')
+            ->join('orderinfo as oi', 'ol.orderinfo_id', '=', 'oi.orderinfo_id')
+            ->select(DB::raw('monthname(oi.date_placed) as month, sum(ol.quantity * i.sell_price) as total'))
+            ->groupBy(DB::raw('monthname(oi.date_placed)'))
+            ->orderBy(DB::raw('month(oi.date_placed)'))
+            ->pluck('total', 'month')
+            ->all();
+
+        // dd($sales);
+        $labels = (array_keys($sales));
+
+        $data = array_values($sales);
+        // dd($sales, $data, $labels);
+        return response()->json(array('data' => $data, 'labels' => $labels));
+    }
+
+    public function itemsChart() {
+        
+        $items = DB::table('item as i')
+                    ->join('orderline as ol', 'i.item_id', '=', 'ol.item_id')
+                    ->select(DB::raw('i.description as items, sum(ol.quantity) as total'))
+                    ->groupBy('i.description')
+                    ->pluck('total','items')
+                    ->all();
+                    
+        // dd($items);
+        $labels = (array_keys($items));
+        
+        $data= array_values($items);
+        // dd($items, $data, $labels);
         return response()->json(array('data' => $data, 'labels' => $labels));
     }
 }
--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -54,4 +54,140 @@ $(document).ready(function () {
             console.log(error);
         }
     });
+
+    $.ajax({
+        type: "GET",
+        url: "/api/dashboard/sales-chart",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            var ctx = $("#salesChart");
+            new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: data.labels,
+                    datasets: [{
+                        label: 'Monthly sales',
+                        data: data.data,
+                        backgroundColor: () => {
+                            //generates random colours and puts them in string
+
+                            var colors = [];
+                            for (var i = 0; i < data.data.length; i++) {
+                                var letters = '0123456789ABCDEF'.split('');
+                                var color = '#';
+                                for (var x = 0; x < 6; x++) {
+                                    color += letters[Math.floor(Math.random() * 16)];
+                                }
+                                colors.push(color);
+                            }
+                            return colors;
+                        },
+
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                },
+            });
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+    $.ajax({
+        type: "GET",
+        url: "/api/dashboard/items-chart",
+        dataType: "json",
+        success: function (data) {
+            console.log(data);
+            var ctx = $("#itemsChart");
+             new Chart(ctx, {
+                type: 'doughnut',
+                data: {
+                    labels: data.labels,
+                    datasets: [{
+                        label: 'number of items sold',
+                        data: data.data,
+
+                        backgroundColor: () => {
+                            //generates random colours and puts them in string
+
+                            var colors = [];
+                            for (var i = 0; i < data.data.length; i++) {
+                                var letters = '0123456789ABCDEF'.split('');
+                                var color = '#';
+                                for (var x = 0; x < 6; x++) {
+                                    color += letters[Math.floor(Math.random() * 16)];
+                                }
+                                colors.push(color);
+                            }
+                            return colors;
+                        },
+                        borderColor: [
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(255,99,132,1)'
+                        ],
+                        borderWidth: 1,
+                        responsive: true,
+                        // hoverBackgroundColor: colors,
+                    }]
+                },
+                options: {
+                    plugins: {
+                        datalabels: {
+                            backgroundColor: function (context) {
+                                return context.dataset.backgroundColor;
+                            },
+                            borderColor: 'white',
+                            borderRadius: 25,
+                            borderWidth: 2,
+                            color: 'white',
+                            display: function (context) {
+                                var dataset = context.dataset;
+                                var count = dataset.data.length;
+                                var value = dataset.data[context.dataIndex];
+                                return value > count * 1.5;
+                            },
+                            font: {
+                                weight: 'bold'
+                            },
+                            padding: 6,
+                            formatter: Math.round
+                        },
+                        aspectRatio: 4 / 3,
+                        cutoutPercentage: 32,
+                        layout: {
+                            padding: 32
+                        },
+                        elements: {
+                            line: {
+                                fill: false
+                            },
+                            point: {
+                                hoverRadius: 7,
+                                radius: 5
+                            }
+                        },
+                    }
+                }
+
+
+            });
+
+        },
+        error: function (error) {
+            console.log(error);
+        }
+    });
+
+
+    
 })
